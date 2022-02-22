513607cca6b1b57ed9f0a49ace8306a6cedca176
Adisa <adisamalik@gmail.com>
2022-02-22 14:04:29 -0500

    Redis fix

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -2,6 +2,10 @@ import {createClient} from 'redis';
 
 const redis = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
 });
 
 (async () => {
