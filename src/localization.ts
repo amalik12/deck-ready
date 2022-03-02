ea047c7b979b07e74f47d0d3e9602c879991b34c
Adisa Malik <adisamalik@gmail.com>
2022-03-01 19:41:35 -0500

    Update localization

--- src/localization.ts
+++ src/localization.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export default async function getLocalization() {
   const steamEngLocalization = await fetch(
-    'https://raw.githubusercontent.com/SteamDatabase/SteamTracking/de8da35bc06bbb8e23e1d2c34070639d5b948bd1/steamcommunity.com/public/javascript/applications/community/localization/shared_english.json'
+    'https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/steamcommunity.com/public/javascript/applications/community/localization/shared_english.json'
   );
   return steamEngLocalization.json() as object;
 }
